4191f986b8b03c9e76a834f19764acefd1a5af37
Molina <molinakmey@gmail.com>
2020-09-19 19:55:17 -0700

    Add build and publish workflow

---
+++ Stormfront Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void loadAScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
