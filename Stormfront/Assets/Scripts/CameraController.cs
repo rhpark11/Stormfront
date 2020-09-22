210c7fda89f1ee323da8e5c2e1d20e9232eb4cc6
Molina <molinakmey@gmail.com>
2020-09-22 13:43:32 -0700

    Stop ignoring ProBuilder Settings json; do not commit this file ever

---
+++ Stormfront/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.position + new Vector3(0.0f, 10.7f, -10.0f);
+        transform.position = Vector3.Lerp(transform.position, playerPos, 0.1f);
+    }
+}
--- Stormfront/Assets/Scripts/PlayerMovement.cs
+++ Stormfront/Assets/Scripts/PlayerMovement.cs
@@ -11,30 +11,35 @@ public class PlayerMovement : MonoBehaviour
     float heading = 0;*/
     [SerializeField]
     Transform cam;
-    
+    [SerializeField]
+    float speed = 5.0f;
     Vector2 input;
     
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+    }
     void Update()
     {
         //Janky camera rotation
         /*heading += Input.GetAxis("Mouse X")*Time.deltaTime*180;
         camPivot.rotation = Quaternion.Euler(0,heading,0);*/
 
-        //Plyaer movement
+        //Player movement
         input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         input = Vector2.ClampMagnitude(input, 1);
         
         //Quick and dirty method to have camera relative movement inputs
         //Another way is to transform input vector into camera relative directions
-        Vector3 camU = cam.up;
+        Vector3 camF = cam.forward;
         Vector3 camR = cam.right;
-        camU.z = 0;
-        camR.z = 0;
-        camU = camU.normalized;
+        camF.y = 0;
+        camF.y = 0;
+        camF = camF.normalized;
         camR = camR.normalized;
 
         //transform.position += new Vector3(input.x,0,input.y)*Time.deltaTime*5;
-        transform.position += (camU*input.y + camR*input.x)*Time.deltaTime*5;
+        transform.position += (camF*input.y + camR*input.x)*Time.deltaTime*speed;
         
     }
 }
